8628d14b4b8c54a59f6dbb86ac099eb3506d2df4
Victor Koome <koomev786@gmail.com>
2024-02-15 20:58:27 +0300

    Add Jsx for the accordion component

---
+++ src/App.js
@@ -0,0 +1,44 @@
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
+];
+
+export default function App() {
+  return (
+    <div>
+      <Accordion />
+    </div>
+  );
+}
+
+function Accordion() {
+  return (
+    <div className="accordion">
+      {faqs.map((faq, i) => (
+        <Item faqObj={faq} index={i} />
+      ))}
+    </div>
+  );
+}
+
+function Item({ faqObj, index }) {
+  return (
+    <div>
+      <p>{index}</p>
+      <h2 className="title">{faqObj.title}</h2>
+      <div className="content-box">
+        <p>{faqObj.text}</p>
+      </div>
+    </div>
+  );
+}
